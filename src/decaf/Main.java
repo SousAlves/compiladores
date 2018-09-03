980e86ac487d674a515e43a281e3a2aa3ffcf781
Leonardo <sousalves.as@gmail.com>
2018-09-03 11:11:30 -0300

    Arruamndo detalhes, falta passar teste tokens 4

--- src/decaf/Main.java
+++ src/decaf/Main.java
@@ -43,7 +43,6 @@ class Main {
 						case DecafLexer.HEXADECIMAL:   
                             				type= " INTLITERAL";
                             				break;
-<<<<<<< HEAD
 						case DecafLexer.FALSE: 
 							type= " BOOLEANLITERAL";
 							break;
@@ -56,9 +55,6 @@ class Main {
 						case DecafLexer.OP:
 							break;
 
-
-=======
->>>>>>> 8bafbd8b1237f6d9a6982f3c74ad5de4d89b17f1
 		        			}
 		        			System.out.println (token.getLine() + type + " " + text);
 		        		}
